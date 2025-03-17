7c690fec9bc3959fa66be42c62cbeea7226177b1
Wagner Ruiz <wagneraguilar.12@gmail.com>
2025-03-16 23:07:46 -0600

    hace los botones con imagenes y tambien estoy creando la segunda pantalla donde se va a poder ver la informacion de los barcos y colocalos tambien y en la tercera ya es la del juego

---
+++ src/main/java/com/mycompany/batallanaval/Partida.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.batallanaval;
+
+/**
+ *
+ * @author wagne
+ */
+public class Partida {
+    
+    private Boolean ganar = true;
+    private Boolean perder = false;
+    private int ronda = 0;
+
+    public Partida() {
+    }
+    
+    
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.batallanaval {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.batallanaval to javafx.fxml;
     exports com.mycompany.batallanaval;
